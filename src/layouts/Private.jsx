fd46ed3d310bf60ec0c1cf7f54682db1b2d1c885
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-01-28 02:27:30 -0200

    Box arredondado de informações pessoais

--- src/layouts/Private.jsx
+++ src/layouts/Private.jsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import ProfileSidebar from '../components/ProfileSidebar'
-
 export default function PrivateLayout({children}) {
-    return (
-      <ProfileSidebar>
-        {children}
-      </ProfileSidebar>
-    )
+  return children
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react'
+import ProfileSidebar from "../components/ProfileSidebar";
+import '../styles/Main.css'
 
 export default class Main extends Component {
   render() {
     return (
-      <div className="columns">
-        <div className="column is-9">
-          <h2>Bem vindo</h2>
-          <p>
-            <span>Recados&nbsp;</span>
-            <span>Fotos&nbsp;</span>
-            <span>Fãs&nbsp;</span>
-            <span>Mensagens&nbsp;</span>
-          </p>
-          <p>
-            <b>Visualizações de perfil:</b> Desde fev. 2016: 0, Semana passada: 0, Ontem: 0
-          </p>
-          <p>
-            <b>Visitantes recentes:</b> Nenhum
-          </p>
-          <p>
-            <b>Sorte de hoje:</b> Você é observador e analítico por natureza
-          </p>
+      <ProfileSidebar>
+        <div className="columns">
+          <div className="column is-9 my-info">
+            <h1>Bem vindo</h1>
+            <p>
+              <span>Recados&nbsp;</span>
+              <span>Fotos&nbsp;</span>
+              <span>Fãs&nbsp;</span>
+              <span>Mensagens&nbsp;</span>
+            </p>
+            <p>
+              <b>Visualizações de perfil:</b> Desde fev. 2016: 0, Semana passada: 0, Ontem: 0
+            </p>
+            <p>
+              <b>Visitantes recentes:</b> Nenhum
+            </p>
+            <p>
+              <b>Sorte de hoje:</b> Você é observador e analítico por natureza
+            </p>
+          </div>
+          <div className="column is-3">
+            <p>Amigos (0)</p>
+            <p>Comunidades (0)</p>
+          </div>
         </div>
-        <div className="column is-3">
-          <p>Amigos (0)</p>
-          <p>Comunidades (0)</p>
-        </div>
-      </div>
+      </ProfileSidebar>
     );
   }
 }
